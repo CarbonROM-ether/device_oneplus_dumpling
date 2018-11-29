b72a009c682087da4e90ee604c5ca8e2e9e9434b
NurKeinNeid <mralexman3000@gmail.com>
2018-11-29 15:42:22 +0100

    dumpling: AOSiP-ify
     * Update dependencies for AOSiP
     * Add Maintainer Prop
     * Enable inline GApps Building

--- aosip.mk
+++ aosip.mk
@@ -21,10 +21,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from dumpling device
 $(call inherit-product, device/oneplus/dumpling/device.mk)
 
-# Inherit some common gzosp stuff.
-$(call inherit-product, vendor/gzosp/config/common_full_phone.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
-PRODUCT_NAME := gzosp_dumpling
+# GApps
+$(call inherit-product-if-exists, vendor/gapps/inline.mk)
+
+# Maintainer Prop
+PRODUCT_BUILD_PROP_OVERRIDES += \
+DEVICE_MAINTAINERS="Alexander Brunswig"
+
+PRODUCT_NAME := aosip_dumpling
 PRODUCT_DEVICE := dumpling
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
